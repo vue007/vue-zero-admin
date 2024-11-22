b10a889e89bc1efd5323b365dfc5a6b50276377d
akai888 <liaokaigogo@foxmail.com>
2024-11-22 12:41:09 +0800

    add view page

---
+++ src/pages/_views/VPage.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="view-page">
+    <header class="page-header" v-if="$slots['header']">
+      <slot name="header"></slot>
+    </header>
+    <section class="page-content">
+      <slot></slot>
+    </section>
+    <div class="page-footer">
+      <slot name="footer"></slot>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+// const exportColumns = ref<any>([])
+// provide('VPage_exportColumns', exportColumns)
+</script>
+
+<style lang="scss" scoped></style>
--- src/pages/_views/ViPage.vue
+++
@@ -1,7 +0,0 @@
-<template>
-  <header>
-    <slot name="header"></slot>
-  </header>
-  <slot></slot>
-  <footer></footer>
-</template>
