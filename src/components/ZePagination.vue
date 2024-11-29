f81b29a0a43cde274a5f72f149d01d059c6c7de0
akai888 <liaokaigogo@foxmail.com>
2024-11-29 12:18:57 +0800

    feat: add pagination component & hook

---
+++ src/components/ZePagination.vue
@@ -0,0 +1,19 @@
+<template>
+  <el-pagination
+    v-model:current-page="pagination.pageNo"
+    :page-size="pagination.pageSize"
+    layout="total, sizes, prev, pager, next,jumper"
+    :page-sizes="pagination.sizeOption"
+    :total="pagination.total"
+    @size-change="(size) => pagination.sizeChange(size)"
+    @current-change="(page) => pagination.currentChange(page)"
+    :small="size !== 'large'"
+  />
+</template>
+
+<script setup lang="ts">
+const size = inject('size')
+const pagination = defineModel<any>()
+</script>
+
+<style lang="scss" scoped></style>
