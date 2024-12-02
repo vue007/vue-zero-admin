90a4448556d358b9828def076ea6ea1ae45e0f84
akai888 <liaokaigogo@foxmail.com>
2024-12-02 12:44:23 +0800

    add component type

--- src/components/types/index.ts
+++ src/components/types/index.ts
@@ -1,5 +1,7 @@
 import type ZeInput from '@/components/ZeInput.vue'
 import type ZeFormItem from '@/components/ZeFormItem.vue'
+import type ZeModal from '../ZeModal.vue'
 
 export type ZeInputInstance = InstanceType<typeof ZeInput>
 export type ZeFormItemInstance = InstanceType<typeof ZeFormItem>
+export type ZeModalInstance = InstanceType<typeof ZeModal>
