97055c5ef3526e16f7bfb7da445963efd406fa1a
akai <liaokaigogo@foxmail.com>
2024-12-10 22:42:07 +0800

    Update BlankLayout.vue

--- src/layouts/BlankLayout.vue
+++ src/layouts/BlankLayout.vue
@@ -3,7 +3,7 @@
     <header class="layout-header">
       <div class="header-left mr-auto" id="header-left">
         <div class="app-logo flex">
-          <img class="w-69 h-36 lt-sm:w-50 lt-sm:h-24" src="@/assets/images/logo.svg" alt="logo" />
+          <img class="w-36 h-36 lt-sm:w-50 lt-sm:h-24" src="@/assets/images/logo.png" alt="logo" />
         </div>
         <div class="app-title ml-16 flex lt-sm:ml-8"></div>
       </div>
